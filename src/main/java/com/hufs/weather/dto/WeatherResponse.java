07e47ede09abddd768610a51b12611dbb9c4332f
y-jh107 <jin99545@naver.com>
2025-05-01 12:34:37 +0900

    Added WeahterResponse Code

--- src/main/java/com/hufs/weather/dto/WeatherResponse.java
+++ src/main/java/com/hufs/weather/dto/WeatherResponse.java
@@ -1,17 +1,15 @@
 package com.hufs.weather.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class WeatherResponse {
     private String date;
     private double maxTemp;
     private double minTemp;
-    private double rain;
-
-    public WeatherResponse(String date, double maxTemp, double rain, double minTemp) {
-        this.date = date;
-        this.maxTemp = maxTemp;
-        this.rain = rain;
-        this.minTemp = minTemp;
-    }
+    private String cond;
 }
