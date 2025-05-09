212fba88430fbd2f05200cb64ee638115d151322
y-jh107 <jin99545@naver.com>
2025-05-09 13:21:59 +0900

    Changed package name and WeatherResponse

--- src/main/java/com/hufs/weather/domain/Weather.java
+++ src/main/java/com/hufs/weather/domain/Weather.java
@@ -1,4 +1,4 @@
-package com.hufs.weather.dto;
+package com.hufs.weather.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class WeatherResponse {
+public class Weather {
     private String date;
     private double maxTemp;
     private double minTemp;
