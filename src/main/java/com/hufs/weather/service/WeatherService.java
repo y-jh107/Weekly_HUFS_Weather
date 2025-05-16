0f740b45a990b2396fbe3ef4a3d1d36a7af48f7c
y-jh107 <jin99545@naver.com>
2025-05-16 09:50:27 +0900

    Pulled changes and edited WeatherService

--- src/main/java/com/hufs/weather/service/WeatherService.java
+++ src/main/java/com/hufs/weather/service/WeatherService.java
@@ -1,8 +1,21 @@
 package com.hufs.weather.service;
 
+import com.hufs.weather.domain.Weather;
+import com.hufs.weather.repository.WeatherRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WeatherService {
 
+    private static WeatherRepository weatherRepository;
+    private static String apiKey = "1dcbe206437fd2070c9fca6ec759e2ef";
+
+    public WeatherService(WeatherRepository weatherRepository) {
+        this.weatherRepository = weatherRepository;
+    }
+
+    public Weather saveWeather(Weather weather) {
+        weatherRepository.save(weather);
+        return weather;
+    }
 }
